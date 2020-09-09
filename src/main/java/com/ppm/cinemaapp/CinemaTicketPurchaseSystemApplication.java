aeed103942c9015d8d63e89c1000160df45ebf98
Paing Pyae Man <paingpyaeman04@gmail.com>
2020-09-09 20:00:04 +0630

    app initialized

---
+++ src/main/java/com/ppm/cinemaapp/CinemaTicketPurchaseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ppm.cinemaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketPurchaseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketPurchaseSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ppm/cinemaapp/CinemaTicketPurchaseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppm.cinemaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketPurchaseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
